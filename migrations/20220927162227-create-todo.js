4a7038bda49e8806583ee92a41235f7dbbf3eac0
GUNDALA-MAHESHWARI <115809613+GUNDALA-MAHESHWARI@users.noreply.github.com>
2023-01-04 06:54:55 -0500

    Update 20220927162227-create-todo.js

--- migrations/20220927162227-create-todo.js
+++ migrations/20220927162227-create-todo.js
@@ -7,24 +7,24 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       dueDate: {
-        type: Sequelize.DATEONLY
+        type: Sequelize.DATEONLY,
       },
       completed: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       }
     });
   },
